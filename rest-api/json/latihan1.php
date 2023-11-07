118442cc736f4a77a9a133c150ebf45e79868990
popo08 <87663454+popo08@users.noreply.github.com>
2023-11-07 09:07:30 +0700

    rest-api

---
+++ rest-api/json/latihan1.php
@@ -0,0 +1,28 @@
+<?php
+
+// $mahasiswa = [
+//     [
+//     "nama" => "Arif Wicaksono",
+//     "nim" => 2000018229,
+//     "j_k" => "L"
+//     ],
+//     [
+//         "nama" => "Wicaksono",
+//         "nim" => "2000017378",
+//         "j_k" => "L"
+//     ]
+// ];
+
+
+
+$dbh = new PDO ('mysql:host=localhost;dbname=db_webservice', 'root',
+'');
+$db = $dbh->prepare('SELECT * FROM mahasiswa');
+$db -> execute();
+$mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
+
+
+$data = json_encode($mahasiswa);
+echo $data;
+
+?>
