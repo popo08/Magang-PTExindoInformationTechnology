b31b6d8efc168a8393d4093cc20217be19da38c1
popo08 <87663454+popo08@users.noreply.github.com>
2023-11-08 09:53:59 +0700

    Update script.js

--- rest-api/pizza/js/script.js
+++ rest-api/pizza/js/script.js
@@ -3,7 +3,16 @@ $.getJSON('data/pizza.json', function(data){
     
     $.each(menu, function(i, data) {
         $('#daftar-menu').append('<div class="col-md-4"><div class="card mb-3"><img src="img/pizza/'+ data.gambar +'" class="card-img-top"><div class="card-body"><h5 class="card-title">'+ data.nama +'</h5><p class="card-text">'+ data.deskripsi +'</p><h5 class="card-title">Rp '+ data.harga +'</h5><a href="#" class="btn btn-primary">Pesan</a></div></div></div>');  
-
+            $('#daftar-menu').append('<div class="col-md-4"><div class="card mb-3"><img src="img/pizza/<?= $row["gambar"]; ?> "  class="card-img-top"><div class="card-body"><h5 class="card-title"><?= $row["nama"]; ?></h5><p class="card-text"><?= $row["deskripsi"]; ?></p><h5 class="card-title">Rp <?= $row["harga"]; ?></h5><a href="#" class="btn btn-primary">Pesan</a></div></div></div>')
     });
 
 });
+
+$('.nav-link').on('click', function (){
+    $('.nav-link').removeClass('active');
+    $(this).addClass('active');
+
+    let kategori = $(this).html();
+    console.log(kategori);
+});
+
